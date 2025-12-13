7beaf4157f18459398c73c4ccf2da53fedede25d
Leo <leocasti2@gmail.com>
2025-12-13 14:50:03 -0500

    feat: introduce ServiceContainer para simplificar instanciar tantos servicios

--- orchestrator.py
+++ orchestrator.py
@@ -5,17 +5,12 @@ from datetime import datetime
 from pathlib import Path
 
 from tvpipe.config import AppConfig, get_config
+from tvpipe.container import ServiceContainer
 from tvpipe.logging_config import setup_logging
-from tvpipe.services.caracoltv import CaracolTVSchedule
-from tvpipe.services.program_monitor import ProgramMonitor
-from tvpipe.services.publisher import EpisodePublisher
 from tvpipe.services.register import RegistryManager
 from tvpipe.services.telegram import TelegramService
 from tvpipe.services.telegram.schemas import UploadedVideo
 from tvpipe.services.watermark import WatermarkService
-from tvpipe.services.youtube.client import YtDlpClient
-from tvpipe.services.youtube.service import YouTubeFetcher
-from tvpipe.services.youtube.strategies import CaracolDesafioParser
 from tvpipe.utils import ReliabilityGuard, sleep_progress
 
 logger = logging.getLogger("Orchestrator")
@@ -94,30 +89,8 @@ def get_or_upload_video(
 def run_orchestrator():
     setup_logging(f"logs/{Path(__file__).stem}.log")
     config = get_config("config.env")
+    services = ServiceContainer(config)
     guard = ReliabilityGuard()
-    register = RegistryManager()
-
-    # Servicios
-    tg_service = TelegramService(
-        session_name=config.telegram.session_name,
-        api_id=config.telegram.api_id,
-        api_hash=config.telegram.api_hash,
-        workdir=config.telegram.to_telegram_working,
-    )
-
-    schedule_client = CaracolTVSchedule()
-    monitor = ProgramMonitor(schedule_client, "desafio")
-    watermark_service = WatermarkService()
-
-    publisher = EpisodePublisher(
-        config=config.telegram,
-        client=tg_service,
-    )
-    yt_client = YtDlpClient()
-    desafio_strategy = CaracolDesafioParser()
-    downloader = YouTubeFetcher(
-        config.youtube, register, desafio_strategy, client=yt_client
-    )
 
     logger.info(">>> SISTEMA INICIADO: Orquestador en control <<<")
     consecutive_errors = 0
@@ -132,41 +105,43 @@ def run_orchestrator():
                     continue
 
                 # Comprobacion de Horario
-                if monitor.should_wait():
-                    monitor.wait_until_release()
+                if services.monitor.should_wait():
+                    services.monitor.wait_until_release()
                     continue
 
-                episode_meta = downloader.fetch_episode()
+                episode_meta = services.downloader.fetch_episode()
                 if not episode_meta:
                     logger.info("Video no disponible aún. Reintentando en 2 minutos...")
                     sleep_progress(120)
                     continue
             else:
-                episode_meta = downloader.fetch_episode()
+                episode_meta = services.downloader.fetch_episode()
                 if episode_meta is None:
                     raise Exception("No se pudo descargar el episodio.")
 
             logger.info(f"Procesando episodio: {episode_meta.title}")
 
             # Descarga de video
-            ep_dled = downloader.download_episode(episode_meta)
-            register.register_downloads(ep_dled.episode_number, ep_dled.video_paths)
+            ep_dled = services.downloader.download_episode(episode_meta)
+            services.register.register_downloads(
+                ep_dled.episode_number, ep_dled.video_paths
+            )
 
             # Descarga de thumbnail
-            thumbnail_path = downloader.download_thumbnail(episode_meta)
+            thumbnail_path = services.downloader.download_thumbnail(episode_meta)
 
             with add_watermark_to_image(
-                thumbnail_path, "https://t.me/DESAFIO_SIGLO_XXI", watermark_service
+                thumbnail_path, "https://t.me/DESAFIO_SIGLO_XXI", services.watermark
             ) as watermarked_thumb:
                 ready_to_publish_list = []
                 for video_path in ep_dled.video_paths:
-                    if register.was_video_uploaded(video_path):
-                        data = register.get_video_uploaded(video_path)
+                    if services.register.was_video_uploaded(video_path):
+                        data = services.register.get_video_uploaded(video_path)
                         chat_id = data["chat_id"]
                         message_id = data["message_id"]
-                        if tg_service.exists_video_in_chat(chat_id, message_id):
+                        if services.tg.exists_video_in_chat(chat_id, message_id):
                             logger.info("Video reutilizado desde caché.")
-                            uploaded_video = tg_service.fetch_video_uploaded(
+                            uploaded_video = services.tg.fetch_video_uploaded(
                                 chat_id, message_id
                             )
                             ready_to_publish_list.append(uploaded_video)
@@ -175,9 +150,9 @@ def run_orchestrator():
                             logger.info(
                                 "Entrada de caché inválida. Limpiando registro."
                             )
-                            register.remove_video_entry(video_path)
+                            services.register.remove_video_entry(video_path)
 
-                    uploaded_video = tg_service.upload_video(
+                    uploaded_video = services.tg.upload_video(
                         video_path=video_path,
                         thumbnail_path=watermarked_thumb,
                         target_chat_id=config.telegram.chat_id_temporary,
@@ -186,13 +161,17 @@ def run_orchestrator():
                     ready_to_publish_list.append(uploaded_video)
                     chat_id = uploaded_video.chat_id
                     message_id = uploaded_video.message_id
-                    register.register_video_uploaded(message_id, chat_id, video_path)
+                    services.register.register_video_uploaded(
+                        message_id, chat_id, video_path
+                    )
 
-                succes = publisher.publish(
+                succes = services.publisher.publish(
                     ep_dled.episode_number, ready_to_publish_list
                 )
                 if succes:
-                    register.register_episode_publication(ep_dled.episode_number)
+                    services.register.register_episode_publication(
+                        ep_dled.episode_number
+                    )
                     logger.info(f"Episodio {ep_dled.episode_number} publicado.")
                     consecutive_errors = 0
                 else:
---
+++ tvpipe/container.py
@@ -0,0 +1,49 @@
+from tvpipe.config import AppConfig
+from tvpipe.services.caracoltv import CaracolTVSchedule
+from tvpipe.services.program_monitor import ProgramMonitor
+from tvpipe.services.publisher import EpisodePublisher
+from tvpipe.services.register import RegistryManager
+from tvpipe.services.telegram import TelegramService
+from tvpipe.services.watermark import WatermarkService
+from tvpipe.services.youtube.client import YtDlpClient
+from tvpipe.services.youtube.service import YouTubeFetcher
+from tvpipe.services.youtube.strategies import CaracolDesafioParser
+
+
+class ServiceContainer:
+    """
+    Clase encargada de ensamblar todas las dependencias del sistema.
+    Centraliza la creación de objetos para limpiar el orquestador.
+    """
+
+    def __init__(self, config: AppConfig):
+        self.register = RegistryManager()
+
+        self.tg = TelegramService(
+            session_name=config.telegram.session_name,
+            api_id=config.telegram.api_id,
+            api_hash=config.telegram.api_hash,
+            workdir=config.telegram.to_telegram_working,
+        )
+
+        self.watermark = WatermarkService()
+
+        self.schedule = CaracolTVSchedule()
+
+        self.monitor = ProgramMonitor(
+            client=self.schedule,
+            program_url_keyword="desafio",  # TODO: Esto deberia ir en config
+        )
+
+        self.publisher = EpisodePublisher(config=config.telegram, client=self.tg)
+
+        # 3. Servicios de Descarga
+        self.yt_client = YtDlpClient()
+        self.strategy = CaracolDesafioParser()
+
+        self.downloader = YouTubeFetcher(
+            config=config.youtube,
+            registry=self.register,
+            episode_parser=self.strategy,
+            client=self.yt_client,
+        )
